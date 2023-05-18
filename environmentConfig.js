837e40e92f06fdc574d70ef891d561e51ea7dfbb
Naveen Kumar Vadla <vnaveenkumar9705@gmail.com>
2023-05-18 20:25:46 +0530

    [#1] Add local db support

--- environmentConfig.js
+++ environmentConfig.js
@@ -1,6 +1,6 @@
 const getPort = () => process.env.PORT || 2000;
 const getHost = () => process.env.HOSTNAME;
 const getBotToken = () => process.env.BOT_TOKEN;
-const getMongoUrl = () => process.env.MONGO_URL;
+const getMongoUrl = () => process.env.MONGO_URL || "mongodb://localhost:30001/?replicaSet=my-replica-set";
 
 module.exports = { getPort, getHost, getBotToken, getMongoUrl };
--- local.js
+++ local.js
@@ -3,9 +3,8 @@ const environmentConfig = require('./environmentConfig');
 
 const port = environmentConfig.getPort();
 
-(async function() {
-  const url = await ngrok.connect(port);
-  const domain = url.replace('https://', '');
-  process.env.HOSTNAME = domain;
+(async function () {
+  const url = await ngrok.connect(port).catch(console.error);
+  process.env.HOSTNAME = url.replace('https://', '');
   require('./server');
 })();
