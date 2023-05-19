d909df71b6822133719ebba36bb58ead89d428c3
Naveen Kumar Vadla <vnaveenkumar9705@gmail.com>
2023-05-19 13:07:32 +0530

    [#1] Add try catch for local server

--- local.js
+++ local.js
@@ -4,7 +4,11 @@ const environmentConfig = require('./environmentConfig');
 const port = environmentConfig.getPort();
 
 (async function () {
-  const url = await ngrok.connect(port).catch(console.error);
-  process.env.HOSTNAME = url.replace('https://', '');
-  require('./server');
+  try {
+    const url = await ngrok.connect(port);
+    process.env.HOSTNAME = url.replace('https://', '');
+    require('./server');
+  } catch (e) {
+    console.log(e);
+  }
 })();
